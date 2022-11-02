34834081cf81aea238f3b407377dff5d16b95036
Alice Shao <a1ice_shao@yahoo.com>
2022-11-01 22:05:05 -0400

    added query to add dept and add role

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,7 +6,7 @@ VALUES ("boy group"),
 INSERT INTO role (title, salary, department_id)
 VALUES ("Stray Kids", 100, 1),
        ("TWICE", 150, 2),
-       ("ITZY", 90, 3);
+       ("DAY6", 70, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Christopher", "Bang", 1, 1),
@@ -15,9 +15,8 @@ VALUES ("Christopher", "Bang", 1, 1),
        ("Nayeon", "Im", 2, 4),
        ("Jeongyeon", "Yoo", 2, 4),
        ("Momo", "Hirai", 2, 4),
-       ("Yeji", "Hwang", 3, 7),
-       ("Julia", "Kim", 3, 7),
-       ("Chaeryeong", "Lee", 3, 7);
+       ("Wonpil", "Lee", 3, 7);
+
 
 
 
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ function startApp() {
             type: "list",
             name: "task",
             message: "What would you like to do?",
-            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "nothing else"]
         }
     ])
     .then((response) => {
@@ -25,16 +25,18 @@ function startApp() {
             db.query("SELECT * FROM role", function (err, results) {
                 console.log(results);
                 console.table("All roles", [...results]);
+                startApp();
             });
         } else if (response.task === "view all employees") {
-            db.query("SELECT id, first_name, last_name FROM employee", function (err, results) {
+            db.query("SELECT * FROM employee JOIN role ON employee.role_id = role.id", function (err, results) {
                 console.log(results);
                 console.table("All employees", [...results]);
+                startApp();
             });
         } else if (response.task === "add a department") {
-
+            addDepartment();
         } else if (response.task === "add a role") {
-
+            addRole();
         } else if (response.task === "add an employee") {
 
         } else if (response.task === "update an employee role") {
@@ -45,6 +47,46 @@ function startApp() {
         }
     })
 }
+
+function addDepartment() {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "dept",
+            message: "What is the name of the department?"
+        }
+    ])
+    .then((response) => {
+        db.query("INSERT INTO department(name) VALUES (?)", response.dept, () => {
+            console.log(`${response.dept} was added to the list of departments.`);
+        })
+        startApp();
+    })
+}
+
+function addRole() {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "role",
+            message: "What is the name of role?"
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the role's salary?"
+        }
+    ])
+    .then((response) => {
+        db.query("INSERT INTO role(title, salary) VALUES (?, ?)", [response.role, response.salary], () => {
+            console.log(`${response.dept} was added to the list of departments.`);
+        })
+        startApp();
+    })
+}
+
 
 startApp()
 
